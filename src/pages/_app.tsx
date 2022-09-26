ffad3c6fab9ba76e55fd1a63f33a7f1c8f41eaff
justinpaulosolo <justinpaulosolo@gmail.com>
2022-09-25 23:08:19 -0700

    trpc hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { withTRPC } from "@trpc/next";
+import { serverRouter } from "../server/router";
+
+import { ServerRouter } from "../server/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default withTRPC<ServerRouter>({
+  config({ ctx }) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : `http://localhost:3000/api/trpc`;
+    return {
+      url,
+      headers: {
+        "x-ssr": "1",
+      },
+    };
+  },
+  ssr: true,
+})(MyApp);
