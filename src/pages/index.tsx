ddf0f6ea573aee5553d516499f01b7fe0922023b
justinpaulosolo <justinpaulosolo@gmail.com>
2022-09-26 00:06:03 -0700

    protected page / dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { ILogin, loginSchema } from "../common/validation/auth";
-import { trpc } from "../common/client/trpc";
 import { signIn } from "next-auth/react";
 
 const Home: NextPage = () => {
